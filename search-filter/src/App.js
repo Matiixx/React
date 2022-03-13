9084cc29d21133f700af597f024aa82b154d8a40
Matiixx <mateuszcichostepski@gmail.com>
2022-03-13 01:15:29 +0100

    Add debouncing

--- search-filter/src/App.js
+++ search-filter/src/App.js
@@ -5,15 +5,33 @@ export default function App() {
 
   const [users, setUsers] = useState([]);
   const [filter, setFilter] = useState("");
-  const [otherThing, setOtherThing] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]);
 
-  const filteredUsers = useMemo(() => {
+  /*const filteredUsers = useMemo(() => {
     const regexp = new RegExp(filter, 'gi');
     console.log(filter)
     return users.filter((user) => {
       const fullName = user.first_name + " " + user.last_name;
       return fullName.match(regexp);
     });
+  }, [users, filter]);*/
+
+  useEffect(() => {
+    let timeoutID = setTimeout(() => {
+      const regexp = new RegExp(filter, 'gi');
+      console.log(regexp);
+      setFilteredUsers(
+        users.filter((user) => {
+          const fullName = user.first_name + " " + user.last_name;
+          return fullName.match(regexp);
+        })
+      );
+    }, 500);
+
+    return () => {
+      clearTimeout(timeoutID);
+    }
+    
   }, [users, filter]);
 
   useEffect(() => {
@@ -23,15 +41,14 @@ export default function App() {
   return (
     <div className='main'>
       <div className='search-box'>
-        <input type={'text'} className='filter-input' onChange={event => {
+        <input type={'text'} placeholder='Search...' className='filter-input' onChange={event => {
           setFilter(event.target.value);
         }} />
-        <input type={'text'} onChange={event => setOtherThing(event.target.value)} />
       </div>
       <div className='user-list'>
         {users && filteredUsers.map((user) => {
           return (
-           <div className='user-div' placeholder='Search...' key={user.id}>{user.first_name}{" "}{user.last_name}</div>
+           <div className='user-div' key={user.id}>{user.first_name}{" "}{user.last_name}</div>
           );
         })}
       </div>
