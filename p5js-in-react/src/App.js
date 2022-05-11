3cdfc8343ffb1ef97228d45bb99a59b5c86e4e53
Matiixx <mateuszcichostepski@gmail.com>
2022-05-11 17:07:59 +0200

    Load local image from file

--- p5js-in-react/src/App.js
+++ p5js-in-react/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sketch from "react-p5";
 
 function App() {
+  let img;
+  let i = 0;
+  let preload = (p5) => {
+    img = p5.loadImage("./like.jpg");
+    // img.hide();
+  };
+
+  let setup = (p5, canvasParentRef) => {
+    let canvas = p5.createCanvas(100, 100).parent(canvasParentRef);
+    let x = (p5.windowWidth - p5.width) / 2;
+    let y = (p5.windowHeight - p5.height) / 2;
+    canvas.position(x, y);
+
+    // img.hide();
+    console.log(i);
+    // img.loadPixels();
+  };
+
+  let draw = (p5) => {
+    p5.background("rgb(0,0,110)");
+    p5.stroke(255);
+    p5.strokeWeight(5);
+    p5.ellipse(10, 10, 10, 10);
+    if (img) p5.image(img, 0, 0, 100, 100);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sketch preload={preload} setup={setup} draw={draw} className="App" />
     </div>
   );
 }
