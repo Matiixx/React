34929c68528425b8d02bc95d0bbf1a7fb54f48e3
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-07-20 17:34:54 +0200

    Handle input and button

--- todo-zustand-app/src/components/TodoForm/TodoForm.js
+++ todo-zustand-app/src/components/TodoForm/TodoForm.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 import "./TodoForm.css";
+import useStore from "../../store/useStore";
 
 export default function TodoForm() {
+  const [newTask, setNewTask] = useState("");
+  const inputRef = useRef();
+  const addToDo = useStore((state) => state.addToDo);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (newTask !== "") {
+      addToDo(newTask);
+      setNewTask("");
+      inputRef.current.classList.remove("p-filled");
+    }
+  };
+
   return (
     <div className="todo-form">
       <span className="p-float-label">
-        <InputText autoComplete="off" id="username" />
+        <InputText
+          id="task-input"
+          autoComplete="off"
+          value={newTask}
+          ref={inputRef}
+          onChange={(e) => {
+            setNewTask(e.target.value);
+          }}
+        />
         <label htmlFor="username">Write new task</label>
       </span>
-      <Button label="Add Task" aria-label="Submit" />
+      <Button label="Add Task" aria-label="Submit" onClick={handleClick} />
     </div>
   );
 }
