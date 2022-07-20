6efae7f6c4de23a7ede8d61a750a7837ecc6909d
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-07-20 16:36:04 +0200

    Create basic structure

--- todo-zustand-app/src/App.js
+++ todo-zustand-app/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+import Header from "./components/Header/Header";
+import TodoForm from "./components/TodoForm/TodoForm";
+import TodosList from "./components/TodosList/TodosList";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Header />
+      <TodoForm />
+      <TodosList />
+    </div>
+  );
 }
 
 export default App;
