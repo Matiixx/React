b56f2ee0504ae5fe2453cd8c5e15f6f4095ea692
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-07-20 16:36:18 +0200

    Create Header component

---
+++ todo-zustand-app/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div>
+        <h3>To-Do App with Zustand</h3>
+      </div>
+    </div>
+  );
+}
