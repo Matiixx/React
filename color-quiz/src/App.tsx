abd5e369a53659434ee9468ae7ff9284a5598184
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-10-18 19:11:12 +0200

    Add points, generate new game after correct answer

--- color-quiz/src/App.tsx
+++ color-quiz/src/App.tsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 
 function App() {
   const [color, setColor] = useState("");
+  const [answers, setAnswers] = useState<string[]>([]);
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean | undefined>(
+    undefined
+  );
+  const [resultPoints, setResultPoints] = useState(0);
 
-  const randomColor = () => {
-    let array = [
+  const randomColor = (): string => {
+    const array = [
       "0",
       "1",
       "2",
@@ -33,28 +38,75 @@ function App() {
     );
   };
 
-  useEffect(() => {
-    setColor(randomColor());
-  }, []);
+  const generateNewGame = (): void => {
+    const generatedColor = randomColor();
+    setColor(generatedColor);
+    setAnswers(
+      [generatedColor, randomColor(), randomColor()].sort(
+        (a, b) => 0.5 - Math.random()
+      )
+    );
+    setIsCorrectAnswer(undefined);
+  };
+
+  const handleClick = (ans: string): void => {
+    if (ans === color) {
+      setIsCorrectAnswer(true);
+      setResultPoints((p) => p + 1);
+      setTimeout(generateNewGame, 1000);
+      return;
+    }
+    setIsCorrectAnswer(false);
+  };
 
   useEffect(() => {
-    if (color) console.log(color);
-  }, [color]);
+    generateNewGame();
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Choose correct color</h2>
+        <h2>Choose correct hex symbol of color</h2>
       </header>
       <main>
+        <h2 className="result-points">Your points: {resultPoints}</h2>
         <div
           className="color-box"
           style={{ backgroundColor: color ? color : "" }}
         ></div>
         <div className="answers-container">
-          <button>{color ? color : "1"}</button>
-          <button>2</button>
-          <button>3</button>
+          <div className="btn-container">
+            {answers &&
+              answers.map((ans) => {
+                return (
+                  <button
+                    key={ans}
+                    onClick={
+                      isCorrectAnswer === undefined || isCorrectAnswer === false
+                        ? () => handleClick(ans)
+                        : () => {}
+                    }
+                    style={isCorrectAnswer === true ? { cursor: "wait" } : {}}
+                  >
+                    {ans}
+                  </button>
+                );
+              })}
+          </div>
+          {isCorrectAnswer !== undefined && (
+            <div
+              className={
+                isCorrectAnswer ? "result-msg correct" : "wrong result-msg"
+              }
+            >
+              {isCorrectAnswer !== null
+                ? isCorrectAnswer === true
+                  ? "Correct"
+                  : "Wrong!"
+                : ""}
+            </div>
+          )}
         </div>
       </main>
     </div>
