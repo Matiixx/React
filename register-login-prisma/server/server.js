9f0f8b3d65c6cc50e9a0ce3e9658a3149224b94f
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-10-19 15:25:48 +0200

    Cors, add login route

--- register-login-prisma/server/server.js
+++ register-login-prisma/server/server.js
@@ -1,11 +1,16 @@
 import fastify from "fastify";
 import dotenv from "dotenv";
 import sensible from "@fastify/sensible";
+import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
+import { loginUser } from "./controllers/users/users.js";
 
 dotenv.config();
 const app = fastify();
 app.register(sensible);
+app.register(cors, {
+  origin: process.env.CLIENT_URL,
+});
 const prisma = new PrismaClient();
 
 app.get("/users", async (req, res) => {
@@ -17,11 +22,13 @@ app.get("/users", async (req, res) => {
   res.status(200).send(users);
 });
 
-async function commitToDb(promise) {
+export async function commitToDb(promise) {
   const [error, data] = await app.to(promise);
   if (error) return app.httpErrors.internalServerError(error.message);
   return data;
 }
+
+app.post("/users/login", loginUser(prisma));
 
 app.listen({ port: process.env.PORT }, () => {
   console.log("🚀 Server ready at: http://localhost:" + process.env.PORT);
