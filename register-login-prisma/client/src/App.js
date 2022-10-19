72650b687ee532148c24a55193a8e8d0d2cc3240
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-10-19 15:35:10 +0200

    Create login form

--- register-login-prisma/client/src/App.js
+++ register-login-prisma/client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import LoginForm from "./components/LoginForm/LoginForm";
+import UserList from "./components/UserList/UserList";
 
 function App() {
   return (
     <div className="App">
       <LoginForm />
+      {/* <UserList /> */}
     </div>
   );
 }
--- register-login-prisma/client/src/components/LoginForm/LoginForm.jsx
+++ register-login-prisma/client/src/components/LoginForm/LoginForm.jsx
@@ -1,17 +1,44 @@
 import React from "react";
+import axios from "axios";
+import { useRef } from "react";
 import "./LoginForm.scss";
+import { useState } from "react";
 
 export default function LoginForm() {
-  const handleLogin = () => {};
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const loginRef = useRef();
+  const passwordRef = useRef();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const loginCredits = {
+      username: loginRef.current.value,
+      password: passwordRef.current.value,
+    };
+    console.log(loginCredits);
+    axios
+      .post("http://localhost:3000/users/login", loginCredits)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch((error) => {
+        setErrorMessage(error.response?.data?.message);
+      });
+  };
 
   return (
     <div>
       <form>
-        <input type={"text"} placeholder="Login..."></input>
-        <input type={"password"} placeholder="Password..."></input>
-        <button onClick={handleLogin}>Login</button>
+        <input type={"text"} placeholder="Login..." ref={loginRef}></input>
+        <input
+          type={"password"}
+          placeholder="Password..."
+          ref={passwordRef}
+        ></input>
+        <button onClick={(e) => handleLogin(e)}>Login</button>
       </form>
-      <p className="error-message"></p>
+      <p className="error-message">{errorMessage}</p>
     </div>
   );
 }
